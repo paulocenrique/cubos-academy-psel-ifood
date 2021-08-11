9471839657d34aef55eb09a7a415cec9a2d0f26b
paulocenrique <paulocenrique@gmail.com>
2021-08-10 23:23:27 -0300

    Solução psel cubos academy

---
+++ Gato.js
@@ -0,0 +1,9 @@
+function solucao(idadeLimite, idadeAtleta) {
+    if (idadeAtleta <= idadeLimite) {
+        console.log("PODE COMPETIR");
+    } else {
+        console.log("GATO");
+    }
+}
+
+solucao(4,3);
